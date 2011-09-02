098eb150252b978665db92b3c4809983b1692ee3
ytfeldrawkcab <ytfeldrawkcab@gmail.com>
2011-09-02 16:58:46 -0400

    lines in between class statements normalized

--- tsd/models.py
+++ tsd/models.py
@@ -7,14 +7,14 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=30, unique=True)
     def __unicode__(self):
         return self.name
-        
+
 class DyeColor(models.Model):
     name = models.CharField(max_length=50)
     def __unicode__(self):
         return self.name
     class Meta:
         ordering = ['name']
-    
+
 class Style(models.Model):
     manufacturer = models.ForeignKey(Manufacturer)
     number = models.CharField('Style Number', max_length=10)
@@ -23,7 +23,7 @@ class Style(models.Model):
     garmentdyeprice = models.ForeignKey('StylePrice', blank=True, null=True, related_name='+', on_delete=models.SET_NULL)
     def __unicode__(self):
         return self.number + ' ' + self.description
-    
+
 class Size(models.Model):
     name = models.CharField(max_length=30)
     abbr = models.CharField(max_length=5)
@@ -32,7 +32,7 @@ class Size(models.Model):
         return self.name
     class Meta:
         ordering = ["sort"]
-    
+
 class StyleSize(models.Model):
     style = models.ForeignKey(Style)
     size = models.ForeignKey(Size)
@@ -41,19 +41,19 @@ class StyleSize(models.Model):
         return self.size.abbr
     class Meta:
         ordering = ["size"]
-    
+
 class StylePrice(models.Model):
     style = models.ForeignKey(Style)
     name = models.CharField(max_length=200)
     basecost = models.DecimalField(max_digits=100, decimal_places=2)
     def __unicode__(self):
         return self.style.number + " " + self.name
-    
+
 class StylePriceAddedCost(models.Model):
     styleprice = models.ForeignKey(StylePrice)
     stylesize = models.ForeignKey(StyleSize)
     addedcost = models.DecimalField(max_digits=100, decimal_places=2)
-    
+
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     defaultmaincontact = models.ForeignKey('CustomerContact', blank=True, null=True, related_name='+', on_delete=models.SET_NULL)
@@ -63,7 +63,7 @@ class Customer(models.Model):
     defaultbillingaddress = models.ForeignKey('CustomerAddress', blank=True, null=True, related_name='+', on_delete=models.SET_NULL)
     def __unicode__(self):
         return self.name
-    
+
 class CustomerContact(models.Model):
     customer = models.ForeignKey(Customer)
     name = models.CharField(max_length=100)
@@ -73,7 +73,7 @@ class CustomerContact(models.Model):
     title = models.CharField(max_length=50, blank=True)
     def __unicode__(self):
         return self.name
-            
+
 class CustomerAddress(models.Model):
     customer = models.ForeignKey(Customer)
     name = models.CharField(max_length=100, blank=True)
@@ -85,33 +85,33 @@ class CustomerAddress(models.Model):
     country = models.CharField(max_length=50, default='United States', blank=True)
     def __unicode__(self):
         return self.name
-        
+
 #class ArtworkTaskName(models.Model):
 #    name = models.CharField(max_length=30)
-    
+
 class Artwork(models.Model):
     customer = models.ForeignKey(Customer)
     name = models.CharField(max_length=60)
     bagnumber = models.IntegerField()
     def __unicode__(self):
         return self.name
-    
+
 #class ArtworkTask(models.Model):
 #    artwork = models.ForeignKey(Artwork)
 #    user = models.ForeignKey(User)
 #    name = models.OneToOneField(ArtworkTaskName)
 #    name = models.CharField(max_length=40)
-    
+
 #class ArtworkTaskActivity(models.Model):
 #    artworktask = models.ForeignKey(ArtworkTask)
-    
+
 class Imprint(models.Model):
     artwork = models.ForeignKey(Artwork)
     name = models.CharField(max_length=60)
     transcendent = models.BooleanField()
     def __unicode__(self):
         return self.name
-    
+
 class Setup(models.Model):
     imprint = models.ForeignKey(Imprint)
     name = models.CharField(max_length=60)
@@ -120,7 +120,7 @@ class Setup(models.Model):
     deprecated = models.BooleanField()
     def __unicode__(self):
         return self.name
-        
+
 class SetupColor(models.Model):
     setup = models.ForeignKey(Setup)
     positivenumber = models.IntegerField()
@@ -130,17 +130,17 @@ class SetupColor(models.Model):
     inkbase = models.CharField(choices=[('WA','WA'), ('RH','RH')], max_length=2)
     screenmesh = models.IntegerField(blank=True, null=True)
     squeegeetype = models.CharField(choices=[('Blue Square','Blue Square')], max_length=20, blank=True)
-    
+
 class SetupFlash(models.Model):
     setup = models.ForeignKey(Setup)
     headnumber = models.IntegerField()
-    
+
 class Location(models.Model):
     name = models.CharField(max_length=100)
     abbr = models.CharField(max_length=6)
     def __unicode__(self):
         return self.name
-    
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer)
     user = models.ForeignKey(User)
@@ -153,12 +153,12 @@ class Order(models.Model):
     billingaddress = models.ForeignKey(CustomerAddress, blank=True, null=True, related_name='+', on_delete=models.SET_NULL)
 
 reversion.register(Order)
-    
+
 class Group(models.Model):
     order = models.ForeignKey(Order)
     name = models.CharField(max_length=60)
     note = models.TextField(blank=True)
-    
+
 class OrderImprint(models.Model):
     #null = True (for imprint) is necessary because select boxes appear to send 'null' when no choice is made causing validation errors otherwise
     imprint = models.ForeignKey(Imprint, blank=True, null=True)
@@ -168,12 +168,12 @@ class OrderImprint(models.Model):
     name = models.CharField(max_length=60, blank=True)
     colorcount = models.IntegerField('Colors')
     specify = models.BooleanField()
-    
+
 class GroupImprint(models.Model):
     group = models.ForeignKey(Group)
     orderimprint = models.ForeignKey(OrderImprint)
     setup = models.ForeignKey(Setup, blank=True, null=True)
-    
+
 class OrderStyle(models.Model):
     order = models.ForeignKey(Order)
     group = models.ForeignKey(Group, blank=True, null=True)
@@ -181,7 +181,7 @@ class OrderStyle(models.Model):
     styleprice = models.ForeignKey(StylePrice)
     garmentdyecolor = models.ForeignKey(DyeColor, blank=True, null=True)
     piecedyecolor = models.CharField(max_length=40, blank=True)
-    
+
 class OrderSize(models.Model):
     orderstyle = models.ForeignKey(OrderStyle)
     stylesize = models.ForeignKey(StyleSize)
@@ -196,24 +196,24 @@ class PriceParameter(models.Model):
     note = models.TextField(blank=True)
     operator = models.CharField(max_length=3)
     value = models.FloatField()
-    
+
 class StylePriceParameter(models.Model):
     priceparameter = models.ForeignKey(PriceParameter)
     style = models.ForeignKey(Style)
     note = models.TextField(blank=True)
     value = models.FloatField()
-    
+
 class OrderPriceParameter(models.Model):
     priceparameter = models.ForeignKey(PriceParameter)
     order = models.ForeignKey(Order)
     note = models.TextField(blank=True)
     value = models.FloatField()
-    
+
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=50)
     def __unicode__(self):
         return self.name
-    
+
 class Service(models.Model):
     servicecategory = models.ForeignKey(ServiceCategory)
     name = models.CharField(max_length=100)
@@ -221,34 +221,34 @@ class Service(models.Model):
     enteredquantity = models.BooleanField()
     def __unicode__(self):
         return self.name
-    
+
 class OrderService(models.Model):
     service = models.ForeignKey(Service)
     order = models.ForeignKey(Order)
     note = models.CharField(max_length=100, blank=True)
     quantity = models.IntegerField(blank=True, null=True)
     specify = models.BooleanField()
-    
+
 class GroupService(models.Model):
     group = models.ForeignKey(Group)
     orderservice = models.ForeignKey(OrderService)
-    
+
 class ColorFamily(models.Model):
     name = models.CharField(max_length=50)
     def __unicode__(self):
         return self.name
-    
+
 class InkBase(models.Model):
     name = models.CharField(max_length=20)
     abbr = models.CharField(max_length=3)
     def __unicode__(self):
         return self.abbr
-    
+
 class InkIngredient(models.Model):
     name = models.CharField(max_length=30)
     def __unicode__(self):
         return self.name
-    
+
 class InkRecipe(models.Model):
     inknumber = models.IntegerField(primary_key=True)
     colorfamily = models.ForeignKey(ColorFamily)
@@ -259,7 +259,7 @@ class InkRecipe(models.Model):
     note = models.TextField(blank=True)
     def __unicode__(self):
         return str(self.inknumber)
-    
+
 class InkRecipeIngredient(models.Model):
     inkrecipe = models.ForeignKey(InkRecipe)
     inkingredient = models.ForeignKey(InkIngredient)
@@ -267,7 +267,7 @@ class InkRecipeIngredient(models.Model):
     sort = models.IntegerField()
     class Meta:
         ordering = ["sort"]
-    
+
 class InkRecipePantone(models.Model):
     inkrecipe = models.ForeignKey(InkRecipe)
     name = models.CharField(max_length=7)
